1f567fb49bb31a4feb3160da3495e45f01ac72bd
gaurav <gaurav.p@prodapt.com>
2023-09-19 15:06:52 +0530

    beforeSwap

---
+++ AnguPro/AngularSwap/cycleAuth/src/main/java/cycleSpring/cycleAuth/CycleShopBusinessException.java
@@ -0,0 +1,7 @@
+package cycleSpring.cycleAuth;
+
+public class CycleShopBusinessException extends RuntimeException{
+    public CycleShopBusinessException(String message) {
+        super(message);
+    }
+}
